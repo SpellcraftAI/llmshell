5e4e22010c55a586387f2ef92f8cf7751558c683
Lewis <lewis@spellcraft.org>
2024-08-26 04:14:11 -0700

    test: IPC shutdown, server stop
    must use Node.JS APIs and pass in process.stdin (not "pipe") to test Ink.

--- src/lib/log.ts
+++ src/lib/log.ts
@@ -4,32 +4,50 @@ import { mkdir, appendFile } from "fs/promises"
 import type { CoreMessage } from "ai"
 
 // date-time file compatible
-const SESSION_ID =
-  new Date()
-    .toLocaleString("en-US", {
-      year: "numeric",
-      month: "2-digit",
-      day: "2-digit",
-      hour: "2-digit",
-      minute: "2-digit",
-      second: "2-digit",
-      hour12: true,
-    })
-    .replace(/[/:,\s]/g, "_")
+const SESSION_ID = new Date().getTime().toString()
+
+export enum LOGFILE {
+  DEBUG = "debug.txt",
+  TRANSCRIPT = "transcript.txt",
+}
 
 const CONFIG_DIR = resolve(homedir(), ".config", "claude_terminal")
 const SESSION_DIR = resolve(CONFIG_DIR, SESSION_ID)
 
-const DEBUG_PATH = resolve(SESSION_DIR, "debug.txt")
-const SESSION_LOG_PATH = resolve(SESSION_DIR, "session.txt")
+const DEBUG_PATH = resolve(SESSION_DIR, LOGFILE.DEBUG)
+const TRANSCRIPT_PATH = resolve(SESSION_DIR, LOGFILE.TRANSCRIPT)
+
+/**
+ * @returns The directory of the most recent session.
+ */
+export const getLastSessionDirectory = async () => {
+  const glob = new Bun.Glob("./*")
+  const scanner = glob.scan({ cwd: CONFIG_DIR, absolute: true, onlyFiles: false })
+  const results = await Array.fromAsync(scanner)
+  
+  const lastDirectory = results.sort().pop()
+  if (!lastDirectory) {
+    throw new Error("No session directories found")
+  }
+
+  return lastDirectory
+}
+
+export const getLastLog = async (type: LOGFILE) => {
+  const lastSessionDirectory = await getLastSessionDirectory()
+  const lastLogPath = resolve(lastSessionDirectory, type)
+  
+  const logFile = Bun.file(lastLogPath)
+  return await new Response(logFile.stream()).text()
+}
 
 const ensureLogsExist = async () => {
   const DEBUG_FILE = Bun.file(DEBUG_PATH)
-  const SESSION_LOG_FILE = Bun.file(SESSION_LOG_PATH)
+  const TRANSCRIPT_FILE = Bun.file(TRANSCRIPT_PATH)
 
   await mkdir(SESSION_DIR, { recursive: true })
 
-  for (const file of [DEBUG_FILE, SESSION_LOG_FILE]) {
+  for (const file of [DEBUG_FILE, TRANSCRIPT_FILE]) {
     const exists = await file.exists()
     if (!exists) {
       await Bun.write(file, "")
@@ -47,16 +65,16 @@ export const sessionLog = async (...messages: CoreMessage[]) => {
   await ensureLogsExist()
   for (const message of messages) {
     if (typeof message.content === "string") {
-      await appendFile(SESSION_LOG_PATH, `[${message.role}]\n${new Date().toISOString()}\n${message.content}\n\n`)
+      await appendFile(TRANSCRIPT_PATH, `[${message.role}]\n${new Date().toISOString()}\n${message.content}\n\n`)
     } else if (Array.isArray(message.content)) {
       for (const content of message.content) {
         switch (content.type) {
         case "text":
-          await appendFile(SESSION_LOG_PATH, `[${message.role}]\n${new Date().toISOString()}\n${content.text}\n\n`)
+          await appendFile(TRANSCRIPT_PATH, `[${message.role}]\n${new Date().toISOString()}\n${content.text}\n\n`)
           break
         
         default:
-          await appendFile(SESSION_LOG_PATH, `[${message.role}]\n${new Date().toISOString()}\n${JSON.stringify(content, null, 2)}\n\n`)
+          await appendFile(TRANSCRIPT_PATH, `[${message.role}]\n${new Date().toISOString()}\n${JSON.stringify(content, null, 2)}\n\n`)
           break
         }
       }
--- test/terminal.test.ts
+++ test/terminal.test.ts
@@ -1,3 +1,4 @@
+import { getLastLog, LOGFILE } from "@/lib/log"
 import { expect, test } from "bun:test"
 import { spawn } from "child_process"
 import os from "os"
@@ -9,30 +10,37 @@ test("terminal", async () => {
   expect(async () => {
     const executable = os.platform() === "win32" ? "./dist/bin.exe" : "./dist/bin"
     console.log({ executable })
+
     const subprocess = spawn(
       executable,
       [],
       {
-        stdio: [process.stdin, "pipe", "pipe"],
+        stdio: [process.stdin, "pipe", "pipe", "ipc"],
         env: { ...process.env, FORCE_COLOR: "1" },
       },
     )
 
-    subprocess.stdout.on("data", (chunk: Buffer) => stdoutBuffer.push(...chunk))
-    subprocess.stderr.on("data", (chunk: Buffer) => stderrBuffer.push(...chunk))
+    subprocess.stdout?.on("data", (chunk: Buffer) => stdoutBuffer.push(...chunk))
+    subprocess.stderr?.on("data", (chunk: Buffer) => stderrBuffer.push(...chunk))
+
+    await new Promise((resolve) => setTimeout(resolve, 1000))
 
     try {
       subprocess.send("STOP_CLAUDE_SERVER")
-      await new Promise((resolve) => setTimeout(resolve, 1000))
+      await new Promise((resolve) => setTimeout(resolve, 500))
       subprocess.kill("SIGINT")
     } catch (e) {}
   }).not.toThrow()
 
-  if (os.platform() !== "darwin") {
-    expect(stdoutBuffer).toMatchSnapshot(os.version())
-    expect(stderrBuffer).toMatchSnapshot(os.version())
-  }
+  expect(stdoutBuffer, "stdout did not match").toMatchSnapshot(os.version())
+  expect(stderrBuffer, "stderr did not match").toMatchSnapshot(os.version())
 
   process.stdout.write("STDOUT:\n")
   process.stdout.write(Buffer.from(stdoutBuffer).toString())
+
+  const lastDebugLog = await getLastLog(LOGFILE.DEBUG)
+  console.log(lastDebugLog)
+
+  expect(lastDebugLog, "should receive ipc stop message").toContain("STOP_CLAUDE_SERVER")
+  expect(lastDebugLog, "should stop server").toContain("Server stopped")
 })
